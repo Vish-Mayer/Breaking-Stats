c39f37f6e18c0f1d353a2af43c7852ec52611c5d
Vish Mayer <vish.mayer@gmail.com>
2021-06-28 14:48:15 +0100

    redering application title

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import {configure,shallow,mount,render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({adapter:new Adapter()})
+
+const setup=()=>{
+
+    let wrapper = shallow(<App/>);
+
+    return {wrapper};
+};
+
+describe('<Info />',()=>{
+    const {wrapper}=setup()
+
+    it('renders the name of the application',()=>{
+        expect(wrapper.find('h1').text()).toContain("Breaking Stats")
+    })
 });
