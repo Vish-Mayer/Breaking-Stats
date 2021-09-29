299e9f031a8098cef5bcd8e9ebfa79251edbbe4f
Vish Mayer <vish.mayer@gmail.com>
2021-09-29 20:37:36 +0100

    edit on formatCard helper

--- src/helpers/formatCard.test.js
+++ src/helpers/formatCard.test.js
@@ -1,11 +1,11 @@
 import { formatCard } from "./formatCard";
 
-it("reuturns col-md-6 if results data length is 2 ", () => {
+it("reuturns col-md-6 if results data length is less or than 2 ", () => {
+  expect(formatCard(1)).toEqual("col-md-6");
   expect(formatCard(2)).toEqual("col-md-6");
 });
 
-it("reuturns col-md-3 if results data length is not 2 ", () => {
-  expect(formatCard(1)).toEqual("col-md-3");
+it("reuturns col-md-3 if results data length is above 2", () => {
   expect(formatCard(3)).toEqual("col-md-3");
   expect(formatCard(4)).toEqual("col-md-3");
   expect(formatCard(5)).toEqual("col-md-3");
